68ac9e7c4e0b6ded4a41836db93f036e34af6aff
andreasandi17010 <20102017@ittelkom-pwt.ac.id>
2023-05-16 11:17:56 +0700

    andrec

---
+++ app/src/main/java/com/example/kelompok13_halodog/PetFoodActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kelompok13_halodog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PetFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet_food)
+    }
+}
---
+++ app/src/main/java/com/example/kelompok13_halodog/ProfileActivity.kt
@@ -0,0 +1,35 @@
+package com.example.kelompok13_halodog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.kelompok13_halodog.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bvMenu.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.Profil -> {
+                    val intent = Intent(this, ProfileActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.Home -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.Lainnya -> {
+                    true
+                }else -> false
+            }
+        }
+
+    }
+}
